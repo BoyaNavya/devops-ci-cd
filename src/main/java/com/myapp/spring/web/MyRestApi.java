ea84193051af5033e3f04fcb2315df8f2ff6fc4f
BoyaNavya <boyanavya>
2022-02-02 21:20:55 +0530

    scripted pipeline

---
+++ src/main/java/com/myapp/spring/web/MyRestApi.java
@@ -0,0 +1,25 @@
+package com.myapp.spring.web;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MyRestApi {
+	
+	@GetMapping
+	public String sayHello(){
+		
+		return "Welcome To World Of Api With Java Spring and Current Date"+LocalDate.now();
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello1(){
+		
+		return "Welcome To World Of Api With Java1 Spring and Current Date"+LocalDate.now();
+	}
+
+}
